513ac939f907d79c87faf2d2e0fea98c0f0f17c2
Klay Clarke <klayaclarke@gmail.com>
2021-12-11 12:45:39 -0500

    display diff gif if user guess is: above, below or right on target

--- main.py
+++ main.py
@@ -18,11 +18,24 @@ def greeting():
 @app.route('/<int:user_guess>')
 def guess(user_guess):
     if user_guess < random_num:
-        return '<h1>Too low</h1>'
+        return '<h1 style="color: red";>Too low</h1>' \
+               '<div style="width:100%;height:0;padding-bottom:125%;position:relative;"><iframe ' \
+               'src="https://giphy.com/embed/jD4DwBtqPXRXa" width="100%" height="100%" style="position:absolute" ' \
+               'frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a ' \
+               'href="https://giphy.com/gifs/work-digging-jD4DwBtqPXRXa">via GIPHY</a></p>'
     elif user_guess > random_num:
-        return '<h1>Too high</h1>'
+        return '<h1 style="color: purple";>Too high</h1>' \
+               '<div style="width:100%;height:0;padding-bottom:94%;position:relative;"><iframe ' \
+               'src="https://giphy.com/embed/3o6ZtaO9BZHcOjmErm" width="100%" height="100%" ' \
+               'style="position:absolute" ' \
+               'frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p>' \
+               '<a href="https://giphy.com/gifs/dog-puppy-fly-3o6ZtaO9BZHcOjmErm">via GIPHY</a></p>'
     elif user_guess == random_num:
-        return '<h1>You found me</h1>'
+        return '<h1 style="color: green";>You found me</h1>' \
+               '<div style="width:100%;height:0;padding-bottom:105%;position:relative;"><iframe ' \
+               'src="https://giphy.com/embed/4T7e4DmcrP9du" width="100%" height="100%" style="position:absolute" ' \
+               'frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a ' \
+               'href="https://giphy.com/gifs/puppy-biscuit-emerging-4T7e4DmcrP9du">via GIPHY</a></p>'
 
 
 if __name__ == '__main__':
